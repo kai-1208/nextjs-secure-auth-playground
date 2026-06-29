558d8f1137d7875193d03b5fc61e6d218ce2b4b8
kai-1208 <tkhski1208@gmail.com>
2026-06-29 18:10:00 +0900

    セキュリティヘッダーの設定 - CSP - クリックジャッキング対策 - MIMEスニッフィング対策

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,47 @@
 import type { NextConfig } from "next";
 
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data: blob:;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: cspHeader.replace(/\s{2,}/g, " ").trim(),
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+];
+
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
