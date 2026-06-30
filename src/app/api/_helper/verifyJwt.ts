268f0736e2f0a565109da450882a2f77df35bc65
kai-1208 <tkhski1208@gmail.com>
2026-06-30 18:14:05 +0900

    jwtヘルパーにcookieの設定を追加

--- src/app/api/_helper/verifyJwt.ts
+++ src/app/api/_helper/verifyJwt.ts
@@ -1,20 +1,26 @@
 import { NextRequest } from "next/server";
 import { jwtVerify } from "jose";
 import { userProfileSchema } from "@/app/_types/UserProfile";
+import { cookies } from "next/headers";
 
 /**
- * Jwt を検証して成功すれば userId を返す
+ * CookieのJwtを検証して成功すれば userId を返す
  */
 
 export const verifyJwt = async (req: NextRequest): Promise<string | null> => {
-  const jwt = req.headers.get("Authorization")?.replace("Bearer ", "");
-  if (!jwt) {
+  const cookieStore = await cookies();
+  const jwt = cookieStore.get("auth_token")?.value;
+
+  const authHeader = req.headers.get("Authorization")?.replace("Bearer ", "");
+  const token = jwt || authHeader;
+
+  if (!token) {
     console.error("JWT is missing in the request headers.");
     return null;
   }
   const secret = new TextEncoder().encode(process.env.JWT_SECRET!);
   try {
-    const { payload } = await jwtVerify(jwt, secret);
+    const { payload } = await jwtVerify(token, secret);
     const userProfile = userProfileSchema.parse(payload);
     return userProfile.id;
   } catch (e) {
